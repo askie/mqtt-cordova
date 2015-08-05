6f7a3e00f96396f1f5951ba4ad5a60f6a61e3713
Bogomir Engel <bogomir.engel@gmx.de>
2015-08-05 18:28:34 +0200

    Properly dealign with callbackContext invocations & plugin context

--- src/android/MqTTPlugin.java
+++ src/android/MqTTPlugin.java
@@ -10,6 +10,7 @@ import org.json.JSONException;
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -26,7 +27,9 @@ public class MqTTPlugin extends CordovaPlugin {
 
 	private static final String LOGTAG = "MQTT Android Cordova Plugin";
 
+	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);
 		assert this.cordova == null;
 		this.cordova = cordova;
 		this.webView = webView;
@@ -44,14 +47,14 @@ public class MqTTPlugin extends CordovaPlugin {
 			String username = args.getString(1);
 			String password = args.getString(2);
 			String topic = args.getString(3);
-			String qos = args.getString(4);
+			int qos = args.getInt(4);
 			String message = args.getString(5);
-
+			Boolean retained = false;
 			if (action.equals("publish")) {
-				this.publish(quietMode, username, password, topic , qos, message, callbackContext);
+				this.publish(topic, qos, message, retained, callbackContext);
 				return true;
 			} else if (action.equals("connect")) {
-				this.subscribe(quietMode, username, password, topic , qos, callbackContext);
+				this.subscribe(topic, qos, callbackContext);
 				return true;
 			}
 		}
@@ -59,54 +62,73 @@ public class MqTTPlugin extends CordovaPlugin {
 	}
 
 	private void connect(String url, String clientId, CallbackContext callbackContext) {
-		client = new MqttAndroidClient(getContext(), url, clientId);
-		client.connect(null, new IMqttActionListener() {
-			@Override
-			public void onSuccess(IMqttToken mqttToken) {
-				String topics = mqttToken.getTopics();
-				callbackContext.success(topics);
-
-				MqttMessage message = new MqttMessage("Hello, I am Android Mqtt Client.".getBytes());
-				message.setQos(2);
-				message.setRetained(false);
-
-				try {
-					client.publish("messages", message);
-				} catch (MqttPersistenceException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (MqttException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-
-			@Override
-			public void onFailure(IMqttToken arg0, Throwable arg1) {
-				String message = arg1.getMessage();
-				callbackContext.error(message);
-			}
-		});
-	}
-
-	private void publish(Boolean quietMode, String username, String password, String topic , String qos, String message, CallbackContext callbackContext) {
-		client.publish(quietMode, username, password, topic , qos, message);
-		if (message != null && message.length() > 0) {
-			String html= "Publish";
-			callbackContext.success(html);
-
-		} else {
-			callbackContext.error("Check your parameters");
+		client = new MqttAndroidClient(webView.getContext(), url, clientId);
+		try {
+			client.connect(null, new ConnectListener());
+		} catch (MqttException e) {
+			callbackContext.error(e.getMessage());
 		}
 	}
-	private void subscribe(Boolean quietMode, String username, String password, String topic , String qos, CallbackContext callbackContext) {
-		client.subscribe(quietMode, username, password, topic , qos, message);
-		if (url != null && url.length() > 0) {
-			String html= "Subscribe";
-			callbackContext.success(html);
 
-		} else {
-			callbackContext.error("Check your parameters");
+	private void publish(String topic, int qos, String message, Boolean retained, CallbackContext callbackContext) {
+		try {
+			IMqttDeliveryToken token = client.publish(topic, message.getBytes(), qos, retained);
+			if (token != null) {
+				callbackContext.success(token.getMessage().toString());
+			} else {
+				callbackContext.error("token is null");
+			}
+		} catch (MqttException e) {
+			callbackContext.error(e.getMessage());
+		}
+
+	}
+
+	private void subscribe(String topic, int qos, CallbackContext callbackContext) {
+		try {
+			IMqttToken token = client.subscribe(topic, qos);
+			if (token != null) {
+				callbackContext.success(token.getTopics().toString());
+			} else {
+				callbackContext.error("token is null");
+			}
+		} catch (MqttException e) {
+			callbackContext.error(e.getMessage());
+		}
+	}
+
+	private class ConnectListener implements IMqttActionListener {
+
+		private CallbackContext callbackContext;
+
+		public void initialize(CallbackContext callbackContext) {
+			this.callbackContext = callbackContext;
+		}
+
+		@Override
+		public void onSuccess(IMqttToken mqttToken) {
+			String[] topics = mqttToken.getTopics();
+			callbackContext.success(topics.toString());
+
+			MqttMessage message = new MqttMessage("Hello, I am Android Mqtt Client.".getBytes());
+			message.setQos(2);
+			message.setRetained(false);
+
+			try {
+				client.publish("messages", message);
+			} catch (MqttPersistenceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (MqttException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void onFailure(IMqttToken arg0, Throwable arg1) {
+			String message = arg1.getMessage();
+			callbackContext.error(message);
 		}
 	}
 }
